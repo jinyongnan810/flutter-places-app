2ee6eb873b4b1fed4e057b1c85c55e15468c40be
Kin Yuunan <yuunan.kin@gmail.com>
2021-10-15 07:47:31 +0900

    save image to storage

--- lib/components/image_input.dart
+++ lib/components/image_input.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
 
 class ImageInput extends StatefulWidget {
   const ImageInput({Key? key}) : super(key: key);
@@ -22,6 +24,11 @@ class _ImageInputState extends State<ImageInput> {
       setState(() {
         _storedImage = File(image.path);
       });
+      final appDir = await syspaths.getApplicationDocumentsDirectory();
+      final fileName = path.basename(image.path);
+      final filePath = '${appDir.path}/$fileName';
+      final savedImage = await image.saveTo(filePath);
+      print('file saved to: $filePath');
     }
   }
 
